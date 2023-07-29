210804f1d804e31952b4a33613331cc0a421bee1
ALIS SANJAYBHAI MANGUKIYA <al819067@dal.ca>
2023-07-29 18:24:00 -0300

    Modify the endpoint

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -37,7 +37,7 @@ function DragDrop() {
       };
 
       const response = await axios.post(
-        `${process.env.REACT_APP_EXTRACT_LAMBDA_URL}`,
+        `${process.env.REACT_APP_EXTRACT_LAMBDA_URL}/extract`,
         requestData,
       );
 
